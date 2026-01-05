e564d2ee363740e90892d575ce1494ea236f6305
jaberswaid <saraalmemar23@gmail.com>
2026-01-06 00:36:12 +0300

    Update week8_graph_clusters.c

--- week8_graph_clusters.c
+++ week8_graph_clusters.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int g[10][10], v[10]={0}, n;
 
+void dfs(int i){
+    v[i]=1; printf("%d ",i);
+    for(int j=0;j<n;j++)
+        if(g[i][j] && !v[j]) dfs(j);
+}
+
+int main(){
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++) scanf("%d",&g[i][j]);
+
+    for(int i=0;i<n;i++)
+        if(!v[i]){
+            printf("Cluster: ");
+            dfs(i); printf("\n");
+        }
+    return 0;
+}
