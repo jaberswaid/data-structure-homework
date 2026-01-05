14b72cb1e7c391babccfff3a5556e27c99bc3271
jaberswaid <saraalmemar23@gmail.com>
2026-01-06 00:29:38 +0300

    Update week4_max_min_heap_simple.c

--- week4_max_min_heap_simple.c
+++ week4_max_min_heap_simple.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+
+int h[50], n = 0;
+
+void insert(int x) {
+    int i = n++;
+    h[i] = x;
+    while (i > 0 && h[(i-1)/2] < h[i]) {
+        int t = h[i];
+        h[i] = h[(i-1)/2];
+        h[(i-1)/2] = t;
+        i = (i-1)/2;
+    }
+}
+
+int main() {
+    int x;
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &x);
+        insert(x);
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", h[i]);
+
+    return 0;
+}
