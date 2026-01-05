4f7bf87a117c11ba3c56f01184c7fac1930c8b3e
jaberswaid <saraalmemar23@gmail.com>
2026-01-06 00:28:33 +0300

    Update week2_tower_of_hanoi.c

--- week2_tower_of_hanoi.c
+++ week2_tower_of_hanoi.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+void move(int n, char s, char d, char a) {
+    if (n == 0) return;
+
+    move(n-1, s, a, d);
+    printf("Disk %d: %c -> %c\n", n, s, d);
+    move(n-1, a, d, s);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    move(n, 'S', 'D', 'A');
+    return 0;
+}
