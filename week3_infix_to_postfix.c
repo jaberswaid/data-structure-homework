f3966b890d6c1abb6fbeb9ca78bfe3f5a07494f1
jaberswaid <saraalmemar23@gmail.com>
2026-01-06 00:29:04 +0300

    Update week3_infix_to_postfix.c

--- week3_infix_to_postfix.c
+++ week3_infix_to_postfix.c
@@ -1,1 +1,37 @@
 
+#include <stdio.h>
+#include <ctype.h>
+
+char st[50];
+int top = -1;
+
+int pri(char c) {
+    if (c == '+' || c == '-') return 1;
+    if (c == '*' || c == '/') return 2;
+    return 0;
+}
+
+int main() {
+    char in[50], out[50];
+    int i = 0, k = 0;
+
+    scanf("%s", in);
+
+    while (in[i]) {
+        if (isalnum(in[i]))
+            out[k++] = in[i];
+        else {
+            while (top != -1 && pri(st[top]) >= pri(in[i]))
+                out[k++] = st[top--];
+            st[++top] = in[i];
+        }
+        i++;
+    }
+
+    while (top != -1)
+        out[k++] = st[top--];
+
+    out[k] = '\0';
+    printf("%s\n", out);
+    return 0;
+}
