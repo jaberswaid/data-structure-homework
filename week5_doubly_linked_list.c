b93628873fbea7ff962a4924c03cf6f28fded6c4
jaberswaid <saraalmemar23@gmail.com>
2026-01-06 00:30:07 +0300

    Update week5_doubly_linked_list.c

--- week5_doubly_linked_list.c
+++ week5_doubly_linked_list.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node {
+    int data;
+    struct node *l, *r;
+};
+
+struct node* head = NULL;
+
+void add(int x) {
+    struct node* n = malloc(sizeof(struct node));
+    n->data = x;
+    n->l = NULL;
+    n->r = head;
+    if (head) head->l = n;
+    head = n;
+}
+
+void show() {
+    struct node* t = head;
+    while (t) {
+        printf("%d ", t->data);
+        t = t->r;
+    }
+}
+
+int main() {
+    add(3); add(6); add(9);
+    show();
+    return 0;
+}
