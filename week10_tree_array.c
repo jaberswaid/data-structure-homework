c861fbd4a5c4ac4f4bd7283ced3998bbc2bc5ec0
jaberswaid <saraalmemar23@gmail.com>
2026-01-06 00:37:59 +0300

    Update week10_tree_array.c

--- week10_tree_array.c
+++ week10_tree_array.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+int t[20], n;
+
+void preorder(int i){
+    if(i>=n) return;
+    printf("%d ",t[i]);
+    preorder(2*i+1); preorder(2*i+2);
+}
+
+int main(){
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&t[i]);
+    preorder(0);
+    return 0;
+}
